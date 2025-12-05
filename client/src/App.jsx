68ed5e2b673061b2cb66521417b330b2154df620
GuedeD <gdfa95@gmail.com>
2025-12-05 14:48:06 +0000

    good one from claude

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     );
   }
 
-  console.log("first 9");
+  console.log("good one ");
   return (
     <>
       <Box minHeight={"100vh"} className={darkMode ? "mode" : ""}>
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 connectDB();
-console.log("first 9");
+console.log("good one ");
 app.use(
   cors({
     origin: [process.env.CLIENT_URL],
