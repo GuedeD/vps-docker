e8fc013dc972f33ef71c7b910bf31d0ca4ffc884
GuedeD <gdfa95@gmail.com>
2025-12-16 12:46:03 +0000

    Production-ready Docker setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     );
   }
 
-  console.log("good one 3");
+  console.log("good one 4");
   return (
     <>
       <Box minHeight={"100vh"} className={darkMode ? "mode" : ""}>
