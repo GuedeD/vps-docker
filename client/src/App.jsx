98d2f5ac3ae26e0af22ba7ec1ba8c520c2baf891
GuedeD <gdfa95@gmail.com>
2025-12-16 17:56:08 +0000

    Fix frontend with Nginx production setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     );
   }
 
-  console.log("it should be good 2");
+  console.log("it should be good 3");
   return (
     <>
       <Box minHeight={"100vh"} className={darkMode ? "mode" : ""}>
