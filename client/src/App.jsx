2c0236ccf1590b3365f769aa20603749c26eae5b
GuedeD <gdfa95@gmail.com>
2025-12-16 18:37:52 +0000

    Update API URL configuration for production2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     );
   }
 
-  console.log("it should be good 4");
+  console.log("it should be good 5");
   return (
     <>
       <Box minHeight={"100vh"} className={darkMode ? "mode" : ""}>
