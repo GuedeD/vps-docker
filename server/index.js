d67dd8dfa949e1d8f35ddd4bb3fd6131e10161d0
GuedeD <gdfa95@gmail.com>
2025-12-16 16:35:52 +0000

    qwen + claude2

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 connectDB();
-console.log("good one 7 ");
+console.log("good one ");
 app.use(
   cors({
     origin: [process.env.CLIENT_URL],
