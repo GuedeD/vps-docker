0470f9fa882ccbb9191ed9ae93e9f8ef1c52cad6
GuedeD <gdfa95@gmail.com>
2025-12-16 19:25:34 +0000

    Fix API proxy configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     );
   }
 
-  console.log("it should be good ééé");
+  console.log("it should be good zqdsfsf");
   return (
     <>
       <Box minHeight={"100vh"} className={darkMode ? "mode" : ""}>
