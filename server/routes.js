bbe6baf75c54db9765c514619d063a2f76d87c1c
GuedeD <gdfa95@gmail.com>
2025-12-17 13:59:22 +0000

    Fix API base URL configuration2

--- server/routes.js
+++ server/routes.js
@@ -24,25 +24,24 @@ const {
 } = require("./controllers/comment-controller");
 
 const router = express.Router();
+router.get("/users/search/:query", auth, searchUser);
+router.get("/post/:id", auth, singlePost);
+// router.get("/me", auth, myInfo);
+router.get("/post", allPost);
+router.get("/user/:id", auth, userDetails);
 
 router.post("/signin", signin);
 router.post("/login", login);
-
-router.get("/user/:id", auth, userDetails);
-router.put("/user/follow/:id", auth, followUser);
-router.put("/update", auth, updateProfile);
-router.get("/users/search/:query", auth, searchUser);
 router.post("/logout", auth, logout);
-router.get("/me", auth, myInfo);
-
 router.post("/post", auth, addPost);
-router.get("/post", allPost);
-router.delete("/post/:id", auth, deletePost);
+router.post("/comment/:id", auth, addComment);
+
+router.put("/update", auth, updateProfile);
+router.put("/user/follow/:id", auth, followUser);
 router.put("/post/like/:id", auth, likePost);
 router.put("/repost/:id", auth, repost);
-router.get("/post/:id", auth, singlePost);
 
-router.post("/comment/:id", auth, addComment);
+router.delete("/post/:id", auth, deletePost);
 router.delete("/comment/:postId/:id", auth, deleteComment);
 
 module.exports = router;
